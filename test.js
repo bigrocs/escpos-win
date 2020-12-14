3485c1e5c99a6dee09c1559f743a8c99ba477e3d
bigrocs <bigrocs@qq.com>
2020-12-14 09:54:01 +0800

    自动提交 git 代码

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 const escpos = require('escpos');
-const path = require('path');
 escpos.Win = require('./index.js');
 
 const device = new escpos.Win(); // 调用顺序 USB LPT1 COM1
@@ -8,20 +7,28 @@ const options = { encoding: "GB18030" /* default */ }
 // encoding is optional
 const printer = new escpos.Printer(device, options);
 
-    const tux = path.join(__dirname, 'tux.png');
-    escpos.Image.load(tux, function (image) {
-
-        device.open(function () {
-
-            printer.hardware('init')
-                .align('ct')
-                .image(image, 's8')
-                .then(() => {
-                    printer.cut().close();
-                });
-
-            // OR non-async .raster(image, "mode") : printer.text("text").raster(image).cut().close();
-
-        });
-
-    });
+device.open(function (error) {
+    printer
+        .hardware('init')
+        // .font('a')
+        // .align('ct')
+        // .style('bu')
+        // .size(1, 1)
+        .text('The quick brown fox jumps over the lazy dog')
+        .text('敏捷的棕色狐狸跳过懒狗')
+        // 00122012140016
+        // .qrcode('00122012140016', 'code39')
+        // .table(["One", "Two", "Three"])
+        // .tableCustom(
+        //     [
+        //         { text: "Left", align: "LEFT", width: 0.33, style: 'B' },
+        //         { text: "Center", align: "CENTER", width: 0.33 },
+        //         { text: "Right", align: "RIGHT", width: 0.33 }
+        //     ],
+        //     { encoding: 'cp857', size: [1, 1] } // Optional
+        // )
+        // .qrcode('ทดสอบ')
+        .qrimage('00122012140016', { type: 'png', mode: 'dhdw',size:2 }).then(()=>{
+            printer.cut().close();
+        })
+});
