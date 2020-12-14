e953954b34f8c158317af857deb90846e93efdf9
bigrocs <bigrocs@qq.com>
2020-12-14 09:06:29 +0800

    自动提交 git 代码

--- test.js
+++ test.js
@@ -16,7 +16,8 @@ device.open(function (error) {
         // .size(1, 1)
         .text('The quick brown fox jumps over the lazy dog')
         .text('敏捷的棕色狐狸跳过懒狗')
-        .barcode('1234567', 'EAN8')
+        // 00122012140016
+        .barcode('00122012140016', 'EAN8')
         // .table(["One", "Two", "Three"])
         // .tableCustom(
         //     [
